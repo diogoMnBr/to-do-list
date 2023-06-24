6c5984bdb1a77dd4ed4129f7c54bb1af45a9e484
Diogo Marques <diogo.ageflex@hotmail.com>
2023-06-23 22:36:04 -0300

    Mongo DB Apresentacao

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+const checkListRouter = require('./src/routes/checklist')
 const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.use('/checklists', checkListRouter); //checklists entra como parametro para busca da rota. só entrarão rotas que estiverem precedidas do nome checklists
+
+
+
+/* const log = (req, res, next) => {
+    console.log(req.body)
+    console.log(`Data: ${Date.now()}`)
+    next();
+}
+
+app.use(log);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Minha Lista de tarefas :)</h1>');
+})
+
+app.get('/json', (req, res) => {
+    console.log(req.body)
+    const {title, done} = {title: 'Tarefa X', done: true};
+    res.json({title, done});
+}) */
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
